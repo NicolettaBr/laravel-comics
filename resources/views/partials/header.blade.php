17269f96f6a021cd6551e9f351e16cadacd51ff7
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-06-08 17:15:44 +0200

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,47 @@
 <header>
+     
+    <div class="top-bar">
+
+        <div class="container">
+            <ul class="menu-top">
+                <li>dc power visa</li>
+                <li>additional dc sites</li>
+            </ul>
+        </div>
     
+    </div>
+
+    <nav>
+        <div class="container">
+
+            <div class="container-items">
+
+                <div class="img-box">
+                    <img src="{{ asset('img/dc-logo.png') }}" alt="logo dc">
+                </div>
+                
+
+                <ul class="menu-nav">
+                    <li>characters</li>
+                    <li class="active">comics</li>
+                    <li>movies</li>
+                    <li>tv</li>
+                    <li>games</li>
+                    <li>collectibles</li>
+                    <li>videos</li>
+                    <li>fans</li>
+                    <li>news</li>
+                    <li>shop</li>
+                </ul>
+
+                <div class=search>
+                    Search
+                </div>
+
+            </div>
+        </div>
+    </nav>
+
+    <div class="banner"></div>
+
 </header>
